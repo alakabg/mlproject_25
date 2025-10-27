3ff0ddbb68d050df78c27511c130599306050372
alakabg <alakalak.bg11@gmail.com>
2025-10-27 14:56:41 -0400

    Model training files

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import dill
 from src.exceptions import CustomException
+from sklearn.metrics import r2_score
 
 
 # Function to save the object to a file (pkl) from data transformation file.
@@ -15,5 +16,26 @@ def save_object(file_path, obj):
         
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            # Train the model
+            model.fit(X_train, y_train)
+
+            # Predicting the test set results
+            y_test_pred = model.predict(X_test)
+
+            # R2 Score
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+        
+        return report
     except Exception as e:
         raise CustomException(e, sys)
