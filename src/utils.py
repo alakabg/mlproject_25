f56538e9a7f3320775c1f78c24e89f651c48e2da
alakabg <alakalak.bg11@gmail.com>
2025-10-28 11:41:32 -0400

    Model training with hyper parameter tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import dill
 from src.exceptions import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 # Function to save the object to a file (pkl) from data transformation file.
@@ -19,14 +20,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # Train the model
+            para = params[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+            
+            # Train the model
+            # model.fit(X_train, y_train)
 
             # Predicting the test set results
             y_test_pred = model.predict(X_test)
